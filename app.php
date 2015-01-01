010ba4d2a839b14d435b0d2c2ea75a861cab3e5d
Lakota Lustig <lakota@podbe.at>
2014-12-31 20:00:16 -0500

    Update app.php

--- app.php
+++ app.php
@@ -52,6 +52,11 @@ $JSONResponse = file_get_contents("https://blockchain.info/api/v2/create_wallet?
 $arr = json_decode($JSONResponse, true);
 $address = $arr['address'];
 $link = $arr['link'];
+$File = "wallets.txt"; 
+$Handle = fopen($File, 'a+');
+$Data = "\n$link"; 
+fwrite($Handle, $Data);  
+fclose($Handle); 
 $notice_text = "This is a multi-part message in MIME format.";
 $plain_text = "You've been tipped! Someone was visiting your website and they really liked it! Well, enough to tip you at least. What is Bitcoin? 'Bitcoin is a digital currency used to pay for a variety of goods and services. In many ways, it works the same as paper money with some key differences. Although physical forms of Bitcoin exist, the currency's primary form is data so you trade it online, peer to peer, using wallet software or an online service. You can obtain Bitcoin's either by trading other money, goods, or services with people who have them or through mining. The mining process involves running software that performs complex mathematical equations for which you're rewarded a very small portion of a Bitcoin. When you actually have some of the currency, you can then use it to purchase anything that accepts it.' -Adam Dachis, Lifehacker Now that you know what Bitcoin is, go ahead and claim your tip! Your default password is changeyourpassword, however we suggest you create a new password as soon as you log in. $link P.S. - If you'd like to learn more about Bitcoin, bitcoin.org is a great resource. You should also check meetup.com to see if there are any Bitcoin meetups near you!";
 $html_text = "<html>
@@ -84,7 +89,7 @@ $html_text = "<html>
         <h1>You've been tipped!</h1><br />
         <p class='lead'>Someone was visiting your website and they really liked it!</p><br />
         <h2>What is Bitcoin?</h2><br /><p class='lead'>'Bitcoin is a digital currency used to pay for a variety of goods and services. In many ways, it works the same as paper money with some key differences. Although physical forms of Bitcoin exist, the currency's primary form is data so you trade it online, peer to peer, using wallet software or an online service. You can obtain Bitcoin's either by trading other money, goods, or services with people who have them or through mining. The mining process involves running software that performs complex mathematical equations for which you're rewarded a very small portion of a Bitcoin. When you actually have some of the currency, you can then use it to purchase anything that accepts it.'<br />-<a href='http://lifehacker.com/5991523/what-is-bitcoin-and-what-can-i-do-with-it'><i>Adam Dachis, Lifehacker</i></a></p><br />
-        <p>Now that you know what Bitcoin is, go ahead and claim your tip! Your default password is <b>changeyourpassword</b>, however we suggest you create a new password as soon as you log in.</p><br />
+        <p>Now that you know what Bitcoin is, go ahead and claim your tip! Your default password is <b>changeyourpassword</b>, however we suggest you create a new password as soon as you log in. <b>If you don't change your password in a week, we'll assume you don't want the tip, and it will be returned to the sender.</b></p><br />
         <p><a class='btn btn-lg btn-success' href='$link' role='button'>Claim your tip</a></p><br />
         <p>P.S. - If you'd like to learn more about Bitcoin, <a href='http://bitcoin.org'>bitcoin.org</a> is a great resource. You should also check <a href='http://meetup.com'>meetup.com</a> to see if there are any Bitcoin meetups near you!</p>
       </div>
